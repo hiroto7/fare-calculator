f9db70d6550383c68a25e0c618152b057236c8cb
hiroto7 <hiroto450@gmail.com>
2019-02-06 11:46:23 +0900

    [Add] ReadonlyDB

--- src/DB.ts
+++ src/DB.ts
@@ -1,4 +1,8 @@
-export default class DB<K, V, TS extends any[]> extends Map<K, V> {
+export interface ReadonlyDB<K, V, TS extends any[]=[]> extends ReadonlyMap<K, V> {
+    get1(key: K, ...args: TS): V;
+}
+
+export default class DB<K, V, TS extends any[]=[]> extends Map<K, V> implements ReadonlyDB<K, V, TS> {
     constructor(private readonly c: (key: K, ...args: TS) => V) { super(); }
 
     get1(key: K, ...args: TS): V {
